440faa1dfd9d699af4fa490b27aabbff5aa6c734
snejana88 <stefanova1319@gmail.com>
2021-08-20 11:44:59 +0200

    create fillable product model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'price',
+        'priceFlash',
+        'author',
+        'collection',
+        'stock',
+        'sku',
+        'categoryMain',
+        'categorySecondary',
+        'description',
+        'rating',
+        'image1',
+        'image2',
+        'image3',
+    ];
 }
+
