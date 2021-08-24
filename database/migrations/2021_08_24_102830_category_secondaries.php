e61a8afbdd40ce3ab423fee332f6f6035e1cff75
Granaino BUAB <josemiguel@quesada.eu>
2021-08-24 13:45:05 +0200

    creation of category_main and category_secondaries migrations, creation of the corresponding models and creation of factories.

---
+++ database/migrations/2021_08_24_102830_category_secondaries.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CategorySecondaries extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('category_secondaries', function (Blueprint $table) {
+            $table->id();
+
+            $table->integer('position');
+            $table->string('category', 50);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
