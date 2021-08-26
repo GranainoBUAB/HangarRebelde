1e440a8504b5f058700e811cc704196976fde799
snejana88 <stefanova1319@gmail.com>
2021-08-26 12:38:18 +0200

    actualizacion

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -4,8 +4,10 @@
     <div class="container d-flex">
 
 
-        <div><br>
-            <a href="#">Siguenos  --></a>
+        <div class="left-link">
+            <a class="follow"href="#">Siguenos ➡️</a>
+            
+
         </div>
 
         <div class="icons d-flex ">
@@ -15,10 +17,9 @@
             <a href=""><img class="icons" src="<?php echo asset('storage/img/discordapp.png')?>" alt=""></a>
             <a href=""><img class="icons" src="<?php echo asset('storage/img/whatsapp.png')?>" alt=""></a>
         </div>
-        <div>
-
-
-            <a href=""><img class="icon_location" src="<?php echo asset('storage/img/comoLlegar.png')?>" alt="">         <br>Location</a>
+        <div class="right-link">
+            <img class="icon_location" src="<?php echo asset('storage/img/comoLlegar.png')?>" alt="">
+            <a class="text_location" href="">Como llegar</a>
         </div>
     </div>
 
