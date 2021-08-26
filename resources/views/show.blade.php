477be664e42e830c57a3233e24de011786ee94c4
Davidsd16 <davidsanchezdiaz72@gmail.com>
2021-08-26 22:40:30 +0200

    styles card show

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -22,31 +22,29 @@
                 </div>
             <div class="separator"></div>
             <img class="icoCard m-1" src="<?php echo asset('storage/img/carrito-negro.png'); ?>" alt="Flaticon">
+            <div class="card-body">
+               {{--  <h5 class="card-title">Id.{{ $product->id }}</h5> --}}
+                <h5 class="card-title">Titulo: {{ $product->title }}</h5>
+                <h5 class="card-title">Description: {{$product->description}}</h5> 
+                <h5 class="card-title">Price: {{ $product->price }}</h5>
+                <h5 class="card-title">Author: {{ $product->author }}</h5>
+                <h5 class="card-title">Editorial: {{ $product->editorial }}</h5>
+             {{--    <h5 class="card-title">IsAvailable: {{ $product->isAvailable }}</h5>
+                <h5 class="card-title">CanReserve: {{ $product->canReserve }}</h5>
+                <h5 class="card-title">CategoryMain: {{ $product->categoryMain }}</h5> --}}
+                <img src="{{ asset('storage') . '/' . $product->image1 }}" width=90 alt="">
+                <img src="{{ asset('storage') . '/' . $product->image2 }}" width=90 alt="">
+                <img src="{{ asset('storage') . '/' . $product->image3 }}" width=90 alt="">
+                <h5 class="card-title">Format: {{ $product->format }}</h5>
+                <h5 class="card-title">Pages: {{ $product->pages }}</h5>
+            </div>
         </div>
+        
 </div>         
-     
-    
-      
-        
-        
 
-                <div class="card-body">
-                    <h5 class="card-title">Id.{{ $product->id }}</h5>
-                    <h5 class="card-title">Titulo: {{ $product->title }}</h5>
-                    <h5 class="card-title">Description: {{$product->description}}</h5> 
-                    <h5 class="card-title">Price: {{ $product->price }}</h5>
-                    <h5 class="card-title">Author: {{ $product->author }}</h5>
-                    <h5 class="card-title">Editorial: {{ $product->editorial }}</h5>
-                    <h5 class="card-title">IsAvailable: {{ $product->isAvailable }}</h5>
-                    <h5 class="card-title">CanReserve: {{ $product->canReserve }}</h5>
-                    <h5 class="card-title">CategoryMain: {{ $product->categoryMain }}</h5>
-                    <img src="{{ asset('storage') . '/' . $product->image1 }}" width=90 alt="">
-                    <img src="{{ asset('storage') . '/' . $product->image2 }}" width=90 alt="">
-                    <img src="{{ asset('storage') . '/' . $product->image3 }}" width=90 alt="">
-                    <h5 class="card-title">Format: {{ $product->format }}</h5>
-                    <h5 class="card-title">Pages: {{ $product->pages }}</h5>
-                </div>
-     
+
+
+
             <center>
                 <br>
                 <br>
