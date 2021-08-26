3b3c1599d5ded22838badc1bb27ab6c1e901e791
Granaino BUAB <josemiguel@quesada.eu>
2021-08-26 16:09:00 +0200

    change card view for product relationed in show.blade

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -2,62 +2,64 @@
 
 @section('content')
 
-<x-header />
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="card" style="width: 18rem;">
-        {{--  @dd($product->image1) --}}
-            <img src="{{ asset('storage').'/'.$product->image1}}"
-            alt="">
-            <div class="card-body">
-                <h5 class="card-title">Id.{{$product->id}}</h5>
-                <h5 class="card-title">Titulo: {{$product->title}}</h5>
-                {{-- <h5 class="card-title">Description: {{$product->description}}</h5> --}}
-                <h5 class="card-title">Price: {{$product->price}}</h5>
-                <h5 class="card-title">Author: {{$product->author}}</h5>
-                <h5 class="card-title">Editorial: {{$product->editorial}}</h5>
-                <h5 class="card-title">IsAvailable: {{$product->isAvailable}}</h5>
-                <h5 class="card-title">CanReserve: {{$product->canReserve}}</h5>
-                <h5 class="card-title">CategoryMain: {{$product->categoryMain}}</h5>
-                <img src="{{ asset('storage').'/'.$product->image1}}" width=90 alt="">
-                <img src="{{ asset('storage').'/'.$product->image2}}" width=90 alt="">
-                <img src="{{ asset('storage').'/'.$product->image3}}" width=90 alt="">
-                <h5 class="card-title">Format: {{$product->format}}</h5>
-                <h5 class="card-title">Pages: {{$product->pages}}</h5>
-                <h3 class="card-title">Productos relacionados:</h3>
+    <x-header />
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="card" style="width: 18rem;">
+                {{-- @dd($product->image1) --}}
+                <img src="{{ asset('storage') . '/' . $product->image1 }}" alt="">
+                <div class="card-body">
+                    <h5 class="card-title">Id.{{ $product->id }}</h5>
+                    <h5 class="card-title">Titulo: {{ $product->title }}</h5>
+                    {{-- <h5 class="card-title">Description: {{$product->description}}</h5> --}}
+                    <h5 class="card-title">Price: {{ $product->price }}</h5>
+                    <h5 class="card-title">Author: {{ $product->author }}</h5>
+                    <h5 class="card-title">Editorial: {{ $product->editorial }}</h5>
+                    <h5 class="card-title">IsAvailable: {{ $product->isAvailable }}</h5>
+                    <h5 class="card-title">CanReserve: {{ $product->canReserve }}</h5>
+                    <h5 class="card-title">CategoryMain: {{ $product->categoryMain }}</h5>
+                    <img src="{{ asset('storage') . '/' . $product->image1 }}" width=90 alt="">
+                    <img src="{{ asset('storage') . '/' . $product->image2 }}" width=90 alt="">
+                    <img src="{{ asset('storage') . '/' . $product->image3 }}" width=90 alt="">
+                    <h5 class="card-title">Format: {{ $product->format }}</h5>
+                    <h5 class="card-title">Pages: {{ $product->pages }}</h5>
+                    <h3 class="card-title">Productos relacionados:</h3>
+                </div>
+            </div>
+            <center>
+                <br>
+                <br>
+                <div>
+                    <h2>Productos relacionados</h2>
+                </div>
+
+            </center>
+            <div class="row justify-content-center">
+
+                @foreach ($productrelations as $productrelation)
+                    <div class="ct-product m-3">
+                        <div class="ct-img">
+                            <a href="{{ route('show', ['id' => $productrelation->id]) }}">
+                                <img class="imgCard" src="{{ asset('storage') . '/' . $productrelation->image1 }}"
+                                    alt="">
+                            </a>
+                        </div>
+                        <div class="ct-info d-flex flex-row align-items-center p-1">
+                            <div class="ct-txt d-flex flex-column justify-content-center">
+                                <div class="txtTitle d-flex flex-row align-items-center">
+                                    <p class="txtInfoTitle m-0">{{ $productrelation->title }} </p>
+                                    <p class="txtPoints m-0">...</p>
+                                </div>
+                                <p class="txtPrice">{{ $productrelation->price }} &#8364</p>
+                            </div>
+                            <div class="separator"></div>
+                            <img class="icoCard m-1" src="<?php echo asset('storage/img/carrito-negro.png'); ?>" alt="Flaticon">
+                        </div>
+                    </div>
+                @endforeach
             </div>
         </div>
-        <center>
-            <div>---------------------------------------------------------------------------------------</div>
-            <div>------------------------------------- Productos relacionados---------------------------</div>
-            <div>---------------------------------------------------------------------------------------</div>
-        </center>
-        <div class="row justify-content-center">
-
-            @foreach ($productrelations as $productrelation)
-                <div class="card" style="width: 18rem;">
-
-                    <img src="{{ asset('storage') . '/' . $productrelation->image1 }}" alt="">
-                    <div class="card-body">
-                        <h5 class="card-title">Id.{{ $productrelation->id }}</h5>
-                        <h5 class="card-title">Titulo: {{ $productrelation->title }}</h5>
-                        <h5 class="card-title">Price: {{ $productrelation->price }}</h5>
-                        <h5 class="card-title">Author: {{ $productrelation->author }}</h5>
-                        <h5 class="card-title">Editorial: {{ $productrelation->editorial }}</h5>
-                        <img src="{{ asset('storage') . '/' . $productrelation->image2 }}" width=90 alt="">
-                        <img src="{{ asset('storage') . '/' . $productrelation->image3 }}" width=90 alt="">
-                        <td>
-                            <a href="{{ route('show', ['id' => $productrelation->id]) }}"><button type="submit"
-                                    class="btn btn-primary">Show</button></a>
-                        </td>
-                    </div>
-                </div>
-            @endforeach
-        </div>
     </div>
-<<<<<<< HEAD
-</div>
-<x-footer/>
-=======
->>>>>>> 6897fb0c4487e927539e1ee753fec9e6dbebccc3
+    <x-footer />
+
 @endsection
