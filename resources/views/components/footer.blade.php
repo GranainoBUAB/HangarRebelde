a3a3576f0f80ee295b4a4df3fd84acf071e39fd0
snejana88 <stefanova1319@gmail.com>
2021-08-31 13:07:11 +0200

    finish Footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,28 +1,27 @@
 <footer class="footer mt-auto py3 bg-light">
 
-    <hr>
-    <div class="container d-flex">
-
-
+<hr>
+<div class="container d-flex">
         <div class="left-link">
-            <a class="follow" href="#">Siguenos</a>
-            <a href=""><img class="icons" src="<?php echo asset('storage/img/right-arrow.svg')?>" alt=""></a>
+            <div class="follow">Siguenos <img class="icon_follow" src="<?php echo asset('storage/img/right-arrow.svg')?>" alt="">
+            </div>
+
         </div>
 
-    <div class="icons d-flex ">
-        <a href=""><img class="icons" src="<?php echo asset('storage/img/twitter.png')?>" alt=""></a>
-        <a href=""><img class="icons" src="<?php echo asset('storage/img/instagram.png')?>" alt=""></a>
-        <a href=""><img class="icons" src="<?php echo asset('storage/img/facebook.png')?>" alt=""></a>
-        <a href=""><img class="icons" src="<?php echo asset('storage/img/discordapp.png')?>" alt=""></a>
-        <a href=""><img class="icons" src="<?php echo asset('storage/img/whatsapp.png')?>" alt=""></a>
+    <div class="icons d-flex">
+        <a href="https://twitter.com/hangarrebelde"><img class="icons" src="<?php echo asset('storage/img/twitter.png')?>" alt=""></a>
+        <a href="https://www.instagram.com/hangarrebelde/"><img class="icons" src="<?php echo asset('storage/img/instagram.png')?>" alt=""></a>
+        <a href="https://www.facebook.com/hangarrebelde/"><img class="icons" src="<?php echo asset('storage/img/facebook.png')?>" alt=""></a>
+        <a href="https://discord.com/invite/4bd5D8T"><img class="icons" src="<?php echo asset('storage/img/discordapp.png')?>" alt=""></a>
+        <a href="https://api.whatsapp.com/send/?phone=674620999&text&app_absent=0"><img class="icons" src="<?php echo asset('storage/img/whatsapp.png')?>" alt=""></a>
+
     </div>
+
     <div class="right-link">
-        <img class="icon_location" src="<?php echo asset('storage/img/comoLlegar.png')?>" alt="">
-        <a class="text_location" href="">Como llegar</a>
-    </div>
 
-
+        <div class="text_location"><img class="icon_location" src="<?php echo asset('storage/img/locacion-orange.png')?>" alt="">Como llegar</div>
     </div>
+</div>
 
 </footer>
 
