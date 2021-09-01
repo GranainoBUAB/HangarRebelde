b0934b86bb1043c93e5f7e458bf47925f0c60f24
Davidsd16 <davidsanchezdiaz72@gmail.com>
2021-09-01 19:00:27 +0200

    inset change dev

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -24,8 +24,33 @@
         </div>
 
         <div class="input-group mb-3">
-          <span class="input-group-text" id="inputGroup-sizing-default">Autor</span>
+          <span class="input-group-text" id="inputGroup-sizing-default">Autor 1</span>
           <input type="text" name="author1" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+        </div>
+
+        <div class="input-group mb-3">
+          <span class="input-group-text" id="inputGroup-sizing-default">Autor 2</span>
+          <input type="text" name="author2" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+        </div>
+
+        <div class="input-group mb-3">
+          <span class="input-group-text" id="inputGroup-sizing-default">Autor 3</span>
+          <input type="text" name="author3" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+        </div>
+
+        <div class="input-group mb-3">
+          <span class="input-group-text" id="inputGroup-sizing-default">Autor 4</span>
+          <input type="text" name="author4" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+        </div>
+
+        <div class="input-group mb-3">
+          <span class="input-group-text" id="inputGroup-sizing-default">Autor 5</span>
+          <input type="text" name="author5" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+        </div>
+
+        <div class="input-group mb-3">
+          <span class="input-group-text" id="inputGroup-sizing-default">Autor 6</span>
+          <input type="text" name="author6" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
         </div>
 
         <div class="input-group mb-3">
