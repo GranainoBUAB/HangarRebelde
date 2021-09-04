13751f16f7c6031bd6b7e12e56c0cf093cfef070
crismouta <82060703+crismouta@users.noreply.github.com>
2021-09-04 12:10:37 +0200

    update tests crud with middleware admin

--- tests/Feature/Products/CrudTest.php
+++ tests/Feature/Products/CrudTest.php
@@ -3,7 +3,9 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
+use App\Models\User;
 use App\Models\Product;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
@@ -15,29 +17,6 @@ class CrudTest extends TestCase
      *
      * @return void
      */
-
-    public function test_a_product_can_be_created(){
-
-        $this->withoutExceptionHandling();
-        
-        Product::factory(1)->create([
-            'id'=> 1,
-            'title'=> 'Test Title',
-            'description'=> 'Test Description'
-        ]);
-
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
-
-        $this->assertCount(1, Product::all());
-
-        $product =  Product::first();
-
-        $this->assertEquals($product->title, 'Test Title');
-        $this->assertEquals($product->description, 'Test Description');
-    }
-
 
     public function test_list_of_products_can_be_retrived(){
 
@@ -52,43 +31,89 @@ class CrudTest extends TestCase
             
     }
 
+    public function test_a_product_can_be_created(){
+
+        $this->withoutExceptionHandling();
+
+        $user = User::factory()->create(['isAdmin'=>true]);
+        $this->actingAs($user);
+        
+        $response = $this->post(route('store') , [
+            'title' => 'new title',
+            'description' => 'new description',
+            'price' => 'new price',
+            'author1' => 'new author1',
+            'editorial' => 'new editorial',
+            'isAvailable' => true,
+            'canReserve' => true,
+            'isbn' => 'new isbn',
+            'categoryMain' => 'new categoryMain',
+            'image1' => 'new image1',
+            'dateSale' => 'new dateSale',
+            'format' => 'new format',
+            'tag1' => 'new tag1',
+            'pages' => 'new pages'
+        ]);
+
+        $this->assertCount(1, Product::all());
+
+        $this->assertEquals(Product::first()->title,'new title');
+
+        /* $user = User::factory()->create(['isAdmin'=>true]);
+        $this->actingAs($user);
+        
+        $product = Product::factory()->create();
+
+        $response = $this->post(route('store'), [$product]???);
+
+        $response->assertStatus(200);
+
+        $this->assertCount(1, Product::all());
+
+        
+        $this->assertDatabaseCount('products',1);  */
+        
+    }
+
     public function test_a_product_can_be_deleted(){
 
         $this->withoutExceptionHandling();
 
-        Product::factory(1)->create([
-            'id'=> 1
-        ]);
+        $product = Product::factory()->create();
 
         $response = $this->get('/');
         //dd($response);
         $response->assertOk();
 
+        $user = User::factory()->create(['isAdmin'=>true]);
+    
+        $this->actingAs($user);
+
         $this->assertCount(1, Product::all());
         
-        $response = $this->delete('/delete/1');
+        $response = $this->delete(route('delete', $product->id));
         //dd($response);
         $this->assertCount(0, Product::all());
     }
 
     public function test_a_product_can_be_updated(){
 
-        Product::factory(1)->create([
-            'id'=> 1,
-            'title'=> 'Test Title'
-        ]);
+        $product = Product::factory()->create();
 
         $response = $this->get('/');
         //dd($response);
         $response->assertOk();
 
+        $user = User::factory()->create(['isAdmin'=>true]);
+    
+        $this->actingAs($user);
+
         $this->assertCount(1, Product::all());
 
-        $response = $this->patch('/products/1', [
+        $response = $this->patch((route('update', $product->id)), [
             'title'=> 'Update Title',
-
         ]);
-        //dd($response);
+    
         $this->assertEquals(Product::first()->title,'Update Title');
     }
 }
