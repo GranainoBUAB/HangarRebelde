1c11d615c6dd875946ac77b29dce8dbd06470748
crismouta <82060703+crismouta@users.noreply.github.com>
2021-09-07 14:08:52 +0200

    create cart migration, controller, route and blade

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -7,11 +7,11 @@ use Illuminate\Support\Facades\DB;
 
 class CartController extends Controller
 {
-    public function getCart($id)
+    public function getCart($user_id)
     {
         $products = DB::table('products')
             ->join('carts', 'products.id', '=', 'carts.product_id')
-            ->where('user_id', '=', $id)
+            ->where('user_id', '=', $user_id)
             ->get();
         
         return view('cart', compact('products'));
