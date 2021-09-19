052a15d942c4c146e90bc2f399ec21a3397f7847
crismouta <82060703+crismouta@users.noreply.github.com>
2021-09-19 17:06:39 +0200

    add search route test

---
+++ tests/Feature/Products/SearchTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\Models\User;
+use App\Models\Product;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class SearchTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+
+    public function test_search_route()
+    {
+        $response = $this->get(route('search'));
+        
+        $response->assertOk();
+
+    }
+
+
+    /* public function test_return_product_found_by_title()
+    {
+        $products = Product::factory()->create(['title'=>'superman']);
+        
+        $response = $this->get(route('search', ['superman']));
+        
+        $response->assertViewHas('superman');
+        
+    } */
+}
