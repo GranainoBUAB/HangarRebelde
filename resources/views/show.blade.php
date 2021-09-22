ff4240f7c725cea0bc347c6b5e533cf45f662e48
Davidsd16 <davidsanchezdiaz72@gmail.com>
2021-09-22 15:33:14 +0200

    start function change image

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -6,26 +6,9 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="d-flex flex-column flex-md-row mt-5 align-items-center align-items-md-start">
-
-
                 <div class="ct-imgShow">
-                    <img class="imgShow" id="uploadPreview1" src="{{ asset('storage') . '/' . $product->image1 }}" alt="">
-                    <button onclick = makeover()> + Imagenes</button>
+                    <img class="imgShow" src="{{ asset('storage') . '/' . $product->image1 }}" alt="">
                 </div>
-
-                <script type ="text/javascript">
-
-                        function @makeover()
-                        {
-                            var elemento = document.getElementById("uploadPreview1");
-
-                            elemento.src = "uploadPreview3";
-                        } 
-                        
-                        
-                        </script> 
-
-
                 <div class="card-body p-0 mx-md-4 my-4 my-md-0 ct-infoShow">
                     <h5 class="card-title font-weight-bold txtTitleShow">{{ $product->title }}</h5>
                     <div class="d-flex flex-row flex-wrap align-items-center">
@@ -98,72 +81,23 @@
                         <h6 class="extraShow font-weight-bold mx-2"> | Categoría Principal:</h6>
                         <p class="card-title extraShow">{{ $product->categoryMain }}</p>
                     </div>
-                    <div class="d-flex flex-row mt-3">
-
-                        
-
-
-                       {{--  <button onclick = "@makeover()"> + Imagenes</button>
-
-                        <script type ="text/javascript">
-
-                        function @makeover()
-                        {
-                            var elemento = document.getElementById("$product->id;");
-
-                            elemento.src = "$product->image2";
-                        } 
-                        
-                        
-                        </script> --}}
-
-
-
-
-                        <{{-- img class="m-1" src="{{ asset('storage') . '/' . $product->image2,}}" id ='$product->image2' width=90 alt="" id = "$product->id;"><br/>
-  --}}
-                       
-                        <img id="uploadPreview1" class="input-select" src="{{ asset('storage') . '/' . $product->image1 }}" width=90 alt="">
-                        <button onclick = makeover()> + Imagenes</button>
-
-
-
-                        <img id="uploadPreview2" class="input-select" src="{{ asset('storage') . '/' . $product->image2 }}" width=90 alt="">
-                        <button onclick = makeover()> + Imagenes</button>
-
-                       {{--  <script>
-
-                        function makeover()
-                        {
-                            var elemento = document.getElementById("$product->image2");
-
-                            elemento.src = "$product->image3";
-                        } 
-                        
-                        
-                        </script> --}}
-
-
-
-                       <img id="uploadPreview3" class="input-select" src="{{ asset('storage') . '/' . $product->image3 }}" width=90 alt="">
-
-
-
-
-
-                        <div class="d-flex flex-column justify-content-end">
+                    <div class="d-flex flex-wrap flex-row mt-3">
+                        <img class="m-1" src="{{ asset('storage') . '/' . $product->image1 }}" width=90 alt="">
+                        <img class="m-1" src="{{ asset('storage') . '/' . $product->image2 }}" width=90 alt="">
+                        <img class="m-1" src="{{ asset('storage') . '/' . $product->image3 }}" width=90 alt="">
+                        <div class="d-flex flex-column justify-content-end m-1">
                             <div class="d-flex flex-row align-items-center flex-wrap">
-                                <h6 class="extraShow font-weight-bold mx-2">Formato:</h6>
+                                <h6 class="extraShow font-weight-bold">Formato:</h6>
                                 <p class="card-title extraShow">{{ $product->format }}</p>
                             </div>
                             <div class="d-flex flex-row align-items-center flex-wrap">
-                                <h6 class="extraShow font-weight-bold mx-2">Páginas:</h6>
+                                <h6 class="extraShow font-weight-bold">Páginas:</h6>
                                 <p class="card-title extraShow">{{ $product->pages }}</p>
                             </div>
                             <div class="d-flex flex-row align-items-center flex-wrap">
 
                                 @if($product->tag1)
-                                <h6 class="extraShow font-weight-bold mx-2">Etiquetas:</h6>
+                                <h6 class="extraShow font-weight-bold">Etiquetas:</h6>
                                 <a href="{{ route('viewByTag', ['tag'=>$product->tag1]) }}" class="text-reset"><h6 class="extraShow mr-1"> {{ $product->tag1}}  </h6></a>
                                 @endif
 
@@ -191,30 +125,46 @@
                 </div>
             @endif
 
-            <div class="row justify-content-center">
-
+            <div class="d-flex justify-content-center align-items-center mt-5">
+                <h2 class="card-title font-weight-bold txtTitleShow">Productos relacionados</h2>
+            </div>
+            <div class="d-flex flex-wrap row justify-content-center my-4 px-xxl-5">
                 @foreach ($productrelations as $productrelation)
-                    <div class="ct-product m-3">
-                        <div class="ct-img">
-                            <a href="{{ route('show', ['id' => $productrelation->id]) }}">
-                                <img class="imgCard" src="{{ asset('storage') . '/' . $productrelation->image1 }}"
-                                    alt="">
-                            </a>
-                        </div>
-                        <div class="ct-info d-flex flex-row align-items-center p-1">
-                            <div class="ct-txt d-flex flex-column justify-content-center">
-                                <div class="txtTitle d-flex flex-row align-items-center">
-                                    <p class="txtInfoTitle m-0">{{ $productrelation->title }} </p>
-                                    <p class="txtPoints m-0">...</p>
-                                </div>
-                                <p class="txtPrice">{{ $productrelation->price }} &#8364</p>
-                            </div>
-                            <div class="separator"></div>
-                            <a href="{{ route('addCart', ['product_id'=>$product->id]) }}">
-                                <img class="icoCard m-1" src="{{url('/img/shopping-cart.svg')}}" alt="Flaticon">
-                            </a>
-                        </div>
+                <div class="ct-product m-lg-4 m-3">
+                    <div class="ct-img">
+                        <a href="{{ route('show', ['id' => $productrelation->id]) }}">
+                            <img class="imgCard" src="{{ asset('storage') . '/' . $productrelation->image1 }}" alt="">
+                        </a>
                     </div>
+                    <div class="ct-info d-flex flex-row align-items-center p-1">
+                        <div class="ct-txt d-flex flex-column justify-content-center">
+                            <div class="txtTitle d-flex flex-row align-items-center">
+                                <p class="txtInfoTitle text-truncate m-0">{{ $productrelation->title }} </p>
+                            </div>
+                            <p class="txtPrice">{{ $productrelation->price }} &#8364</p>
+                        </div>
+                        <div class="d-flex align-items-center justify-content-center">
+                            <div class="separator">|</div>
+                        </div>
+                        @if ($productrelation->isAvailable == 1)
+                            <a href="{{ route('addCart', ['product_id'=>$productrelation->id]) }}">
+                                <img class="icoCard ml-2" src="{{url('/img/shopping-cart.svg')}}" alt="Flaticon">
+                            </a>
+                        @else
+                            <img class="icoCardNoAvailable ml-2" src="{{url('/img/cartNoAvailable.svg')}}" alt="Flaticon">
+                        @endif
+                    </div>
+                    @if(Auth::check() && Auth::user()->isadmin())
+                        <div class="input-group mb-3">
+                            <a href="{{ route('edit', ['id'=>$productrelation->id]) }}"><button type="text" class="input-group-text">Editar</button></a>
+                            <form action="{{ route('delete', ['id'=>$productrelation->id])}}" method="post">
+                                @method('delete')
+                                @csrf
+                                <input type="submit" class="input-group-text ml-2" onclick="return confirm('¿Estás seguro de que quieres eliminar este producto? {{ $productrelation->title }}')" value="Eliminar">
+                            </form>
+                        </div>
+                    @endif
+                </div>
                 @endforeach
             </div>
         </div>
